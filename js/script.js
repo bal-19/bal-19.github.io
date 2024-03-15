a2c3af04aa375420dc8fb570df28d369a54a49b5
Iqbal Haidee <iqbalhaidee19@gmail.com>
2024-03-15 15:55:59 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// script for navbar section
+const list = document.querySelectorAll(".list");
+function activeLink() {
+  list.forEach((item) => item.classList.remove("active"));
+  this.classList.add("active");
+}
+list.forEach((item) => item.addEventListener("click", activeLink));
